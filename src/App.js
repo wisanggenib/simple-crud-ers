79be8e96eb255d4f32ac57588f21f8d0369fb641
wisanggenib <bagus.wisanggeni4897@gmail.com>
2021-07-01 09:18:36 +0700

    CREATE and REATE DATA

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 
 import sendAsync from "./connector/render";
 
 const App = () => {
-  const [message, setMessage] = useState("SELECT * FROM PRODUK");
+  const [data, setData] = useState("");
   const [response, setResponse] = useState();
 
   function send(sql) {
     sendAsync(sql).then((result) => setResponse(result));
   }
+
+  useEffect(() => {
+    send("SELECT * FROM PRODUK");
+    console.log("Fetch");
+  }, []);
+
+  // useEffect(() => {
+  //   send("SELECT * FROM PRODUK")
+  //   console.log("FETCED")
+  // });
+
   return (
     <div className="App">
       <input
         type="text"
-        value={message}
-        onChange={({ target: { value } }) => setMessage(value)}
+        value={data}
+        onChange={({ target: { value } }) => setData(value)}
       />
-      <button type="button" onClick={() => send(message)}>
+      <button type="button" onClick={() => send(`INSERT INTO PRODUK (PRODUK) VALUES ('${data}')`)}>
         Send
       </button>
-      <pre>
+      {/* <pre>
         {(response && JSON.stringify(response, null, 2)) ||
           "No query results yet!"}
-      </pre>
+      </pre> */}
+      <h3>TABLE OF PRODUK</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>ID PRODUK</th>
+            <th>NAMA PRODUK</th>
+          </tr>
+        </thead>
+        <tbody>
+          {(response &&
+            response.map((item, i) => {
+              return (
+                <Fragment key={item.ID}>
+                  <tr>
+                    <td>{item.ID}</td>
+                    <td>{item.PRODUK}</td>
+                  </tr>
+                </Fragment>
+              );
+            }))}
+        </tbody>
+      </table>
     </div>
   );
 };
